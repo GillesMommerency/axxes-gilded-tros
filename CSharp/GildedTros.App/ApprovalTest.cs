7d6e9061c243ccbc35a2fd39e3bee03593032180
Gilles Mommerency <gilles.mommerency@hotmail.com>
2025-01-08 20:49:30 +0100

    [Task] Adding unit tests + integration to verify existing code

--- CSharp/GildedTros.App/ApprovalTest.cs
+++ CSharp/GildedTros.App/ApprovalTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using ApprovalTests;
@@ -10,17 +11,76 @@ namespace GildedTros.App
     [UseReporter(typeof(DiffReporter))]
     public class ApprovalTest
     {
+        //[Fact]
+        //public void ThirtyDays()
+        //{
+        //    var fakeoutput = new StringBuilder();
+        //    Console.SetOut(new StringWriter(fakeoutput));
+        //    Console.SetIn(new StringReader("a\n"));
+
+        //    Program.Main(new string[] { });
+        //    var output = fakeoutput.ToString();
+
+        //    Approvals.Verify(output);
+        //}
+
         [Fact]
-        public void ThirtyDays()
+        public void AllItems_Should_MatchFinalValues_AfterThirtyDays()
         {
-            var fakeoutput = new StringBuilder();
-            Console.SetOut(new StringWriter(fakeoutput));
-            Console.SetIn(new StringReader("a\n"));
+            #region Arrange
+            // same as in main
+            IList<Item> items = new List<Item>
+            {
+                new Item {Name = "Ring of Cleansening Code", SellIn = 10, Quality = 20},
+                new Item {Name = "Good Wine", SellIn = 2, Quality = 0},
+                new Item {Name = "Elixir of the SOLID", SellIn = 5, Quality = 7},
+                new Item {Name = "B-DAWG Keychain", SellIn = 0, Quality = 80},
+                new Item {Name = "B-DAWG Keychain", SellIn = -1, Quality = 80},
+                new Item {Name = "Backstage passes for Re:factor", SellIn = 15, Quality = 20},
+                new Item {Name = "Backstage passes for Re:factor", SellIn = 10, Quality = 49},
+                new Item {Name = "Backstage passes for HAXX", SellIn = 5, Quality = 49},
+                new Item {Name = "Duplicate Code", SellIn = 3, Quality = 6},
+                new Item {Name = "Long Methods", SellIn = 3, Quality = 6},
+                new Item {Name = "Ugly Variable Names", SellIn = 3, Quality = 6}
+            };
 
-            Program.Main(new string[] { });
-            var output = fakeoutput.ToString();
+          
+            // final values
+            var expectedAfter30Days = new[]
+            {
+                new { Name = "Ring of Cleansening Code", FinalSellIn = -20, FinalQuality = 0 },
+                new { Name = "Good Wine", FinalSellIn = -28, FinalQuality = 31 },
+                new { Name = "Elixir of the SOLID", FinalSellIn = -25, FinalQuality = 0 },
+                new { Name = "B-DAWG Keychain", FinalSellIn = 0, FinalQuality = 80 },
+                new { Name = "B-DAWG Keychain", FinalSellIn = -1, FinalQuality = 80 },
+                new { Name = "Backstage passes for Re:factor", FinalSellIn = -15, FinalQuality = 0 },
+                new { Name = "Backstage passes for Re:factor", FinalSellIn = -20, FinalQuality = 0 },
+                new { Name = "Backstage passes for HAXX", FinalSellIn = -25, FinalQuality = 0 },
+                new { Name = "Duplicate Code", FinalSellIn = -27, FinalQuality = 0 },
+                new { Name = "Long Methods", FinalSellIn = -27, FinalQuality = 0 },
+                new { Name = "Ugly Variable Names", FinalSellIn = -27, FinalQuality = 0 }
+            };
 
-            Approvals.Verify(output);
+            var app = new GildedTros(items);
+            #endregion
+
+            #region Act
+            // sim 30 days
+            for (int day = 0; day < 30; day++)
+            {
+                app.UpdateQuality();
+            }
+            #endregion
+
+            #region Assert
+            // Verify each item matches the expected final SellIn/Quality
+            for (int i = 0; i < items.Count; i++)
+            {
+                Assert.Equal(expectedAfter30Days[i].Name, items[i].Name);
+                Assert.Equal(expectedAfter30Days[i].FinalSellIn, items[i].SellIn);
+                Assert.Equal(expectedAfter30Days[i].FinalQuality, items[i].Quality);
+            }
+            #endregion
         }
     }
 }
--- CSharp/GildedTros.App/GildedTrosTest.cs
+++ CSharp/GildedTros.App/GildedTrosTest.cs
@@ -5,13 +5,197 @@ namespace GildedTros.App
 {
     public class GildedTrosTest
     {
-        [Fact]
-        public void foo()
+        //  Regular Items
+        [Theory]
+        [InlineData("Ring of Cleansening Code", 10, 20, 9, 19)]
+        [InlineData("Elixir of the SOLID", 5, 7, 4, 6)]
+        [InlineData("foo", 0, 0, -1, 0)] // degrade can't go below 0
+        [InlineData("foo", 0, 10, -1, 8)] // degrade 2 after SellIn < 0
+        public void RegularItems_DegradeProperly_OneUpdate(
+            string name,
+            int initialSellIn,
+            int initialQuality,
+            int expectedSellIn,
+            int expectedQuality)
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
-            GildedTros app = new GildedTros(Items);
+            #region Arrange
+            IList<Item> items = new List<Item>
+            {
+                new Item { Name = name, SellIn = initialSellIn, Quality = initialQuality }
+            };
+            var app = new GildedTros(items);
+            #endregion
+
+            #region Act
             app.UpdateQuality();
-            Assert.Equal("fixme", Items[0].Name);
+            #endregion
+
+            #region Assert
+            Assert.Equal(expectedSellIn, items[0].SellIn);
+            Assert.Equal(expectedQuality, items[0].Quality);
+            #endregion
+        }
+
+        // Good Wine
+        [Theory]
+        [InlineData("Good Wine", 2, 0, 1, 1)]   // increases by 1
+        [InlineData("Good Wine", 0, 10, -1, 11)] // "once the sell by date has passed, quality DEGRADES twice as fast so should not double in this instance"
+        [InlineData("Good Wine", 0, 50, -1, 50)] // never exceed 50
+        public void GoodWine_IncreasesQualityPerRequirements(
+            string name,
+            int initialSellIn,
+            int initialQuality,
+            int expectedSellIn,
+            int expectedQuality)
+        {
+            #region Arrange
+            IList<Item> items = new List<Item>
+            {
+                new Item { Name = name, SellIn = initialSellIn, Quality = initialQuality }
+            };
+            var app = new GildedTros(items);
+            #endregion
+
+            #region Act
+            app.UpdateQuality();
+            #endregion
+
+            #region Assert
+            Assert.Equal(expectedSellIn, items[0].SellIn);
+            Assert.Equal(expectedQuality, items[0].Quality);
+            #endregion
+        }
+
+        // B-DAWG Keychain (Legendary)
+        [Theory]
+        [InlineData("B-DAWG Keychain", 0, 80)]
+        [InlineData("B-DAWG Keychain", -1, 80)]
+        [InlineData("B-DAWG Keychain", 5, 80)]
+        public void LegendaryItem_BDawgKeychain_NeverChanges(
+            string name,
+            int initialSellIn,
+            int initialQuality)
+        {
+            #region Arrange
+            IList<Item> items = new List<Item>
+            {
+                new Item { Name = name, SellIn = initialSellIn, Quality = initialQuality }
+            };
+            var app = new GildedTros(items);
+            #endregion
+
+            #region Act
+            app.UpdateQuality();
+            #endregion
+
+            #region Assert
+            Assert.Equal(initialSellIn, items[0].SellIn);
+            Assert.Equal(initialQuality, items[0].Quality);
+            #endregion
+        }
+
+        // Backstage Passes
+        [Theory]
+        [InlineData("Backstage passes for Re:factor", 15, 20, 14, 21)]
+        [InlineData("Backstage passes for Re:factor", 10, 45, 9, 47)]  // +2
+        [InlineData("Backstage passes for Re:factor", 5, 45, 4, 48)]   // +3
+        [InlineData("Backstage passes for Re:factor", 1, 49, 0, 50)]   // cap at 50
+        [InlineData("Backstage passes for Re:factor", 0, 49, -1, 0)]   // after concert =>0
+
+        [InlineData("Backstage passes for HAXX", 15, 20, 14, 21)]
+        [InlineData("Backstage passes for HAXX", 10, 45, 9, 47)]
+        [InlineData("Backstage passes for HAXX", 5, 45, 4, 48)]
+        [InlineData("Backstage passes for HAXX", 1, 49, 0, 50)]
+        [InlineData("Backstage passes for HAXX", 0, 49, -1, 0)]
+        public void BackstagePasses_QualityBehavior(
+            string name,
+            int initialSellIn,
+            int initialQuality,
+            int expectedSellIn,
+            int expectedQuality)
+        {
+            #region Arrange
+            IList<Item> items = new List<Item>
+            {
+                new Item { Name = name, SellIn = initialSellIn, Quality = initialQuality }
+            };
+            var app = new GildedTros(items);
+            #endregion
+
+            #region Act
+            app.UpdateQuality();
+            #endregion
+
+            #region Assert
+            Assert.Equal(expectedSellIn, items[0].SellIn);
+            Assert.Equal(expectedQuality, items[0].Quality);
+            #endregion
+        }
+
+        // Smelly Items
+        [Theory]
+        [InlineData("Duplicate Code", 3, 6, 2, 4)]  // degrade 2 => 4
+        [InlineData("Duplicate Code", 0, 6, -1, 2)] // degrade 4 => 2
+        [InlineData("Long Methods", 3, 6, 2, 4)]
+        [InlineData("Long Methods", 0, 6, -1, 2)]
+        [InlineData("Ugly Variable Names", 3, 6, 2, 4)]
+        [InlineData("Ugly Variable Names", 0, 6, -1, 2)]
+        public void SmellyItems_DegradeTwiceAsFast(
+            string name,
+            int initialSellIn,
+            int initialQuality,
+            int expectedSellIn,
+            int expectedQuality)
+        {
+            #region Arrange
+            IList<Item> items = new List<Item>
+            {
+                new Item { Name = name, SellIn = initialSellIn, Quality = initialQuality }
+            };
+            var app = new GildedTros(items);
+            #endregion
+
+            #region Act
+            app.UpdateQuality();
+            #endregion
+
+            #region Assert
+            Assert.Equal(expectedSellIn, items[0].SellIn);
+            Assert.Equal(expectedQuality, items[0].Quality);
+            #endregion
+        }
+
+        //Edge Cases
+        [Theory]
+        [InlineData("Elixir of the SOLID", 0, 0, -1, 0)]  // cant go below 0
+        [InlineData("Elixir of the SOLID", 0, 1, -1, 0)]  // degrade 2 => 0
+        [InlineData("Good Wine", 0, 50, -1, 50)]          // cantt exceed 50
+        [InlineData("Backstage passes for Re:factor", 1, 50, 0, 50)] // if already 50 => remains 50
+        [InlineData("Backstage passes for Re:factor", 0, 50, -1, 0)] // after show => 0
+        [InlineData("B-DAWG Keychain", 0, 80, 0, 80)]     // always 80
+        public void QualityBoundaries_AndSpecialItems(
+            string name,
+            int initialSellIn,
+            int initialQuality,
+            int expectedSellIn,
+            int expectedQuality)
+        {
+            #region Arrange
+            IList<Item> items = new List<Item>
+            {
+                new Item { Name = name, SellIn = initialSellIn, Quality = initialQuality }
+            };
+            var app = new GildedTros(items);
+            #endregion
+
+            #region Act
+            app.UpdateQuality();
+            #endregion
+
+            #region Assert
+            Assert.Equal(expectedSellIn, items[0].SellIn);
+            Assert.Equal(expectedQuality, items[0].Quality);
+            #endregion
         }
     }
 }
