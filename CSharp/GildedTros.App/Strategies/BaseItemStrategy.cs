815480bd77b44106922cd37071cd07fd1ed6e7e6
Gilles Mommerency <gilles.mommerency@hotmail.com>
2025-01-08 22:25:03 +0100

    [fix] force git adding strategies that were ignored for some reason

---
+++ CSharp/GildedTros.App/Strategies/BaseItemStrategy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GildedTros.App.Strategies
+{
+    public abstract class BaseItemStrategy : IItemUpdateStrategy
+    {
+        public void UpdateItem(Item item)
+        {
+           // item specific logic
+            UpdateItemCore(item);
+
+            // boundaries enforcing
+            EnforceBoundaries(item);
+        }
+
+        protected abstract void UpdateItemCore(Item item);
+
+        protected virtual void EnforceBoundaries(Item item)
+        {
+            if (item.Quality < 0) item.Quality = 0;
+            if (item.Quality > 50) item.Quality = 50;
+        }
+    }
+}
---
+++ CSharp/GildedTros.App/Strategies/SmellyItemStrategy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GildedTros.App.Strategies
+{
+    internal class SmellyItemStrategy : BaseItemStrategy
+    {
+        protected override void UpdateItemCore(Item item)
+        {
+            item.SellIn--;
+
+            // degrace twice as fast
+            int degrade = (item.SellIn < 0) ? 4 : 2;
+
+            item.Quality -= degrade;
+        }
+    }
+}
